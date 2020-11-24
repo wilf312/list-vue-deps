51a52eb6d9c9a00dca7f17ccd2f03ad21a21c98f
OkadaGenya <g.okada.wilf+github@gmail.com>
2020-11-24 15:22:52 +0900

    Update find.js

--- find.js
+++ find.js
@@ -1,5 +1,11 @@
+/**
+ * 依存ファイルの探索用
+ */
 const path = require('path')
 const fs = require('fs')
+const webpackAlias = require('./webpack.config').resolve.alias
+
+console.log(webpackAlias)
 
 const filePath = 'src/components/pages/users-show/index.vue'
 const dir = __dirname
@@ -22,17 +28,28 @@ if (!file) {
 
   // scriptタグ内を取得
   const script = file.split('<script>')[1].split('</script>')[0]
-  console.log(script)
+  // console.log(script)
+  // importとfromが含まれる行を抽出
   const importLine = script
     .split('\n')
     .filter(
       line => line.indexOf('import') !== -1 && line.indexOf('from') !== -1
     )
-  const re = /from '([@|/|a-z|-|.]*)'/
 
-  const file
+  // ファイルパスを抽出
+  // @doc https://regexper.com/#%2Ffrom%20'%28%5B%40%7C%2F%7Ca-z%7C%5C-%7C.%5D*%29'%2F
+  const re = /from '([@|/|a-z|\-|.]*)'/
+  const aaa = importLine.map(d => d.match(re)[1])
+  // console.log( JSON.stringify(aaa, null, 2))
 
-  console.log(importLine)
+  // aliasを解決
+  const keys = Object.keys(webpackAlias)
+  const resolvedAlias = aaa.map(d => {
+    keys[0]
+    return
+  })
+
+
 }
 
 // console.log(file)
