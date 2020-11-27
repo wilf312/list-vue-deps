d790f8cf143d1d830c98e315be15f588c5589676
OkadaGenya <g.okada.wilf+github@gmail.com>
2020-11-27 10:10:09 +0900

    add example project

---
+++ examples/vue-cli-project/src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="onClick">{{text}}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String
+  },
+  methods: {
+    onClick() {
+      this.$emit('click')
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
